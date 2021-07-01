ed0fd0722740544f753578df809720b883dde264
MuriloCarmo <murilo1258@hotmail.com>
2021-06-30 23:06:03 -0300

    Commit Menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,10 @@ import SearchIcon from "@material-ui/icons/Search";
 import LinkedinLogo from "../../images/linkedin.png";
 import HeaderOptions from "./HeaderOptions";
 import HomeIcon from "@material-ui/icons/Home";
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import
-
-
-
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
 	return (
@@ -23,15 +22,19 @@ function Header() {
 			</div>
 
 			<div className="header__right">
-                <HeaderOptions Icon={HomeIcon} title="Home"/>
-                <HeaderOptions Icon={SupervisorAccountIcon} title="My Network"/>
-                <HeaderOptions Icon={BusinessCenterIcon} title="Jobs"/>
-                <HeaderOptions Icon={ChatIcon} title="Messaging"/>
-                <HeaderOptions Icon={NotificationsIcon} title="Notifications"/>
-
-
-
-            </div>
+				<HeaderOptions Icon={HomeIcon} title="Home" />
+				<HeaderOptions
+					Icon={SupervisorAccountIcon}
+					title="My Network"
+				/>
+				<HeaderOptions Icon={BusinessCenterIcon} title="Jobs" />
+				<HeaderOptions Icon={ChatIcon} title="Messaging" />
+				<HeaderOptions Icon={NotificationsIcon} title="Notifications" />
+				<HeaderOptions
+					avatar="https://secure.gravatar.com/avatar/6b87b87d75353ff2bc3fd782fda436e0?s=64"
+					title="Me"
+				/>
+			</div>
 		</div>
 	);
 }
